7b22d93da6788b044c134abcf69536dd3a5be88b
Ethan Crawford <ecrawford4@ewu.edu>
2023-04-05 10:43:57 -0700

    added equals, toString, hashCode, getters

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -1,5 +1,7 @@
 package cscd212classes.lab2;
 
+import java.util.Objects;
+
 public class Television {
     private final boolean fourK;
     private final String make;
@@ -23,4 +25,40 @@ public class Television {
         this.screenSize = screenSize;
         this.smart = smart;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Television that = (Television) o;
+        return fourK == that.fourK && resolution == that.resolution && screenSize == that.screenSize && smart == that.smart && Objects.equals(make, that.make) && Objects.equals(model, that.model);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fourK, make, model, resolution, screenSize, smart);
+    }
+
+    @Override
+    public String toString() {
+        String str = make + '-' + model + ", " + screenSize + " inch " + resolution;
+        return str;
+    }
 }
+
