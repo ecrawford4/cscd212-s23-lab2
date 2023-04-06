1c3d01492ac53ca934ba18822cb10cbb00b4482e
Ethan Crawford <ecrawford4@ewu.edu>
2023-04-05 22:36:12 -0700

    added exceptions, some comments

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -11,21 +11,36 @@ public class Television {
     private final boolean smart;
 
     public Television(String model, boolean smart, int screenSize, int resolution, String make) {
+        if(model == null || make == null || screenSize < 32 || resolution < 720)
+            throw new IllegalArgumentException("Bad parameters Television EVC");
+
         this.model = model;
         this.smart = smart;
         this.screenSize = screenSize;
         this.resolution = resolution;
         this.make = make;
+
+        if(resolution == 2160)
+            fourK = true;
+        else fourK = false;
     }
 
     public Television(String make, String model, boolean smart, int screenSize, int resolution) {
+        if(model == null || make == null || screenSize < 32 || resolution < 720)
+            throw new IllegalArgumentException("Bad parameters Television EVC");
+
         this.make = make;
         this.model = model;
         this.resolution = resolution;
         this.screenSize = screenSize;
         this.smart = smart;
+
+        if(resolution == 2160)
+            fourK = true;
+        else fourK = false;
     }
 
+    //look this over
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,11 +67,13 @@ public class Television {
         return screenSize;
     }
 
+    //need to finish this
     @Override
     public int hashCode() {
         return Objects.hash(fourK, make, model, resolution, screenSize, smart);
     }
 
+    //need to finish this
     @Override
     public String toString() {
         return make + '-' + model + ", " + screenSize + " inch " + resolution;
