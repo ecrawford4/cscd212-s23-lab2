20dad4f16079d2445cc6357d849a42e96353a6b2
Ethan Crawford <ecrawford4@ewu.edu>
2023-04-07 11:05:11 -0700

    fixed TelevisionModelScreenSizeComparator

--- src/cscd212comparators/lab2/TelevisionModelScreenSizeComparator.java
+++ src/cscd212comparators/lab2/TelevisionModelScreenSizeComparator.java
@@ -8,8 +8,8 @@ public class TelevisionModelScreenSizeComparator implements Comparator<Televisio
     public int compare(final Television t1, final Television t2) {
         if (t1 == null || t2 == null)
             throw new IllegalArgumentException("null parameter in TelevisionModelScreenSizeComparator");
-        if(t1.getModel().compareTo(t2.getModel()) != 0)
-            return Integer.compare(t1.getScreenSize(),t2.getScreenSize());
+        if(t1.getModel().compareTo(t2.getModel()) == 0)
+            return (t1.getScreenSize() - t2.getScreenSize());
         return t1.getModel().compareTo(t2.getModel());
     }
 }
